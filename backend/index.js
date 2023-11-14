cd6a55a245579c53ea03ff33ee506c28c9dba94a
baloghbe <92228708+baloghbe@users.noreply.github.com>
2023-11-14 19:29:34 +0100

    Book creation
    POST method tested in Postman

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,42 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response)=> {
     console.log(request)
     return response.status(234).send('haha yes')
 });
 
 
+app.post('/books', async (request, response)=> {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
 
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+        
+    }
+});
 
 
 
